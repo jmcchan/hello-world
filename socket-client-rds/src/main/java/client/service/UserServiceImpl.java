3d278f14fcee02152d079385bc260c79b849536b
MyeongchanJin <jmcchan@naver.com>
2020-02-06 15:04:17 +0900

    MVC 모델에 맞게 회원 가입에 필요한 로직 작성

--- socket-client-rds/src/main/java/client/service/UserServiceImpl.java
+++ socket-client-rds/src/main/java/client/service/UserServiceImpl.java
@@ -1,5 +1,49 @@
 package client.service;
 
-public class UserServiceImpl implements UserService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import client.repository.mapper.UserMapper;
+import client.repository.vo.UserVo;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+	private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+	@Autowired
+	UserMapper userMapper;
+	
+	@Override
+	public boolean setUser(UserVo uvo) {
+		uvo.setVisit(1L);
+        int insertedCount = userMapper.insertUser(uvo);
+        logger.debug("Join Method Called in UserService");
+        
+        return 1 == insertedCount;
+	}
+
+	@Override
+	public UserVo getUserByEmailAndPasswordAtService(String email, String password) {
+        logger.debug("UserSelectedByEmailAndPassword Called Using UserMapper");
+        logger.debug("id:" + email + " - Service");
+        logger.debug(("pw:" + password + " - Service"));
+        UserVo user = userMapper.selectUserByEmailAndPasswordAtMapper(email, password);
+        logger.debug("Reverse");
+        if (user != null) {
+            logger.debug("id : " + user.getEmail());
+            logger.debug("pw : " + user.getPassword());
+            logger.debug("name: " + user.getName());
+            user.setVisit(user.getVisit() + 1);
+            logger.debug("visited : " + user.getVisit());
+        } else {
+            logger.debug("user : null");
+        }
+
+        logger.debug("UserSelectedByEmailAndPassword in UserService : " + user);
+        return user;
+	}
+	
+	
 }
